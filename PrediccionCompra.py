21eecc1c8a556e558fc8039d8fada868714c2256
DESKTOP-CETU301\Jotaro <ezdi169@vidalibarraquer.net>
2026-05-13 16:39:30 +0200

    git push origin main

--- PrediccionCompra.py
+++ PrediccionCompra.py
@@ -1,7 +1,7 @@
 # Librerias importadas
 import numpy as np
 import pandas as pd
-import matplotlib as plt
+import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score, classification_report
